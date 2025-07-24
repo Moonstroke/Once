79a9b1f8665d476c3e25d7e58b156ffec1a517ad
Joachim MARIE <moonstroke+github@live.fr>
2025-07-24 17:37:02 +0200

    Add methods to the class
    Also, the class is made generic.

--- src/main/java/io/github/moonstroke/once/OnceSettableField.java
+++ src/main/java/io/github/moonstroke/once/OnceSettableField.java
@@ -3,6 +3,13 @@ package io.github.moonstroke.once;
 /**
  * A special container for a single value, allowing only a single initialization.
  */
-public class OnceSettableField {
+public class OnceSettableField<T> {
 
+	public void set(T value) {
+		throw new UnsupportedOperationException("Not implemented"); // TODO
+	}
+
+	public T get() {
+		throw new UnsupportedOperationException("Not implemented"); // TODO
+	}
 }
