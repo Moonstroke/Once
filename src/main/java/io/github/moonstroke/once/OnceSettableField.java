b225d61e68dffc846ed88faffb69ad9a8e98fee1
Joachim MARIE <moonstroke+github@live.fr>
2025-07-24 17:41:18 +0200

    Document the methods

--- src/main/java/io/github/moonstroke/once/OnceSettableField.java
+++ src/main/java/io/github/moonstroke/once/OnceSettableField.java
@@ -5,10 +5,25 @@ package io.github.moonstroke.once;
  */
 public class OnceSettableField<T> {
 
+	/**
+	 * Initialize the instance's value, or fail if has already been set.
+	 *
+	 * @param value The value to set
+	 *
+	 * @throws IllegalStateException if the value has already been initialized
+	 * @throws NullPointerException  if value is {@code null}
+	 */
 	public void set(T value) {
 		throw new UnsupportedOperationException("Not implemented"); // TODO
 	}
 
+	/**
+	 * Retrieve the instance's value, or fail if it has not been set.
+	 *
+	 * @return the value set
+	 *
+	 * @throws IllegalStateException if the value was not initialized
+	 */
 	public T get() {
 		throw new UnsupportedOperationException("Not implemented"); // TODO
 	}
