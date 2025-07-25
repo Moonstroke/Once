90b8e84275a97e69bf75c2778be014e4e6941aa9
Joachim MARIE <moonstroke+github@live.fr>
2025-07-25 22:09:22 +0200

    Overload conversion method to accept a custom msg

--- src/main/java/io/github/moonstroke/once/Requirement.java
+++ src/main/java/io/github/moonstroke/once/Requirement.java
@@ -33,4 +33,20 @@ public interface Requirement<T> {
 	public static <U> Requirement<U> fromPredicate(Predicate<U> predicate) {
 		throw new UnsupportedOperationException("Not implemented"); // TODO
 	}
+
+	/**
+	 * Wrap a boolean predicate into a requirement instance, specifying a custom error message.
+	 *
+	 * @param <U>       The type of the value checked
+	 * @param predicate The boolean predicate to wrap
+	 * @param message   The error message to throw if the requirement is not met
+	 *
+	 * @return A requirement instance performing the check represented by the given predicate
+	 *
+	 * @throws NullPointerException     if predicate or message is {@code null}
+	 * @throws IllegalArgumentException if message is empty
+	 */
+	public static <U> Requirement<U> fromPredicate(Predicate<U> predicate, String message) {
+		throw new UnsupportedOperationException("Not implemented"); // TODO
+	}
 }
