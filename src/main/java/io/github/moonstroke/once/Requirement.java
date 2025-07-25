b724b18f05782412df56f26247649ead1d8311a5
Joachim MARIE <moonstroke+github@live.fr>
2025-07-25 21:51:30 +0200

    Make the interface functional
    This will allow to initialize it from lambda expressions or method
    references.

--- src/main/java/io/github/moonstroke/once/Requirement.java
+++ src/main/java/io/github/moonstroke/once/Requirement.java
@@ -3,6 +3,7 @@ package io.github.moonstroke.once;
 /**
  * An additional requirement that the field value must meet when it is being set.
  */
+@FunctionalInterface
 public interface Requirement<T> {
 
 	/**
