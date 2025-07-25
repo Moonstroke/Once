c662b9cb68421c11ff5351a8a200bd1d13f3e4b5
Joachim MARIE <moonstroke+github@live.fr>
2025-07-25 21:50:03 +0200

    Add a method to the interface
    The method allows to perform the actual check that the obejct
    represents.

--- src/main/java/io/github/moonstroke/once/Requirement.java
+++ src/main/java/io/github/moonstroke/once/Requirement.java
@@ -4,4 +4,13 @@ package io.github.moonstroke.once;
  * An additional requirement that the field value must meet when it is being set.
  */
 public interface Requirement<T> {
+
+	/**
+	 * Perform the check that this requirement represents.
+	 *
+	 * @param value The value to check
+	 *
+	 * @throws IllegalArgumentException if the value does not meet the requirement
+	 */
+	void check(T value) throws IllegalArgumentException;
 }
