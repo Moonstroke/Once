4de05dfbe9c71cee3753037f8475ec822a2a3227
Joachim MARIE <moonstroke+github@live.fr>
2025-08-02 18:41:04 +0200

    Document (succinctly) the requirement instances

--- src/main/java/io/github/moonstroke/once/Requirements.java
+++ src/main/java/io/github/moonstroke/once/Requirements.java
@@ -14,18 +14,33 @@ public class Requirements {
 	}
 
 
+	/**
+	 * Require a string to be non-empty.
+	 */
 	public static final Requirement<String> STRING_NOT_EMPTY = Requirement.fromPredicate(str -> !str.isEmpty(),
 	                                                                                     "value cannot be an empty string");
 
+	/**
+	 * Require a string to contain at least one non-whitespace character.
+	 */
 	public static final Requirement<String> STRING_NOT_BLANK = Requirement.fromPredicate(str -> !str.isBlank(),
 	                                                                                     "value cannot be a blank string");
 
+	/**
+	 * Require that a list be non-empty.
+	 */
 	public static final Requirement<List<?>> LIST_NOT_EMPTY = Requirement.fromPredicate(list -> !list.isEmpty(),
 	                                                                                    "value cannot be an empty list");
 
+	/**
+	 * Require that a set be not empty.
+	 */
 	public static final Requirement<Set<?>> SET_NOT_EMPTY = Requirement.fromPredicate(set -> !set.isEmpty(),
 	                                                                                  "value cannot be an empty set");
 
+	/**
+	 * Require a collection to be not empty.
+	 */
 	public static final Requirement<Collection<?>> COLLECTION_NOT_EMPTY = Requirement.fromPredicate(coll -> !coll.isEmpty(),
 	                                                                                                "value cannot be an empty collection");
 }
