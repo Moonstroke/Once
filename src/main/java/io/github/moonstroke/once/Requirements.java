eca178455151e66a324784f331a992b318a0aa33
Joachim MARIE <moonstroke+github@live.fr>
2025-08-02 18:36:32 +0200

    Add size requirements for collections
    There are three requirements added: one for lists, one for sets, and one
    for collections. The  only difference between them, and the reason for
    their existence (as in fact the collection requirement can be used for
    lists and sets), resides in the fact that the custom error message
    explicitly mentions the collection type.

--- src/main/java/io/github/moonstroke/once/Requirements.java
+++ src/main/java/io/github/moonstroke/once/Requirements.java
@@ -1,5 +1,9 @@
 package io.github.moonstroke.once;
 
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+
 /**
  * Defines a set of common requirements, for various types.
  */
@@ -15,4 +19,13 @@ public class Requirements {
 
 	public static final Requirement<String> STRING_NOT_BLANK = Requirement.fromPredicate(str -> !str.isBlank(),
 	                                                                                     "value cannot be a blank string");
+
+	public static final Requirement<List<?>> LIST_NOT_EMPTY = Requirement.fromPredicate(list -> !list.isEmpty(),
+	                                                                                    "value cannot be an empty list");
+
+	public static final Requirement<Set<?>> SET_NOT_EMPTY = Requirement.fromPredicate(set -> !set.isEmpty(),
+	                                                                                  "value cannot be an empty set");
+
+	public static final Requirement<Collection<?>> COLLECTION_NOT_EMPTY = Requirement.fromPredicate(coll -> !coll.isEmpty(),
+	                                                                                                "value cannot be an empty collection");
 }
