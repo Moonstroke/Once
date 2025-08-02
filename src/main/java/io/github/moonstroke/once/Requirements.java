f9d00a9105612c6715e2220af62013ce09dc1d51
Joachim MARIE <moonstroke+github@live.fr>
2025-08-02 17:11:45 +0200

    Add a utilities class defining common requirements

---
+++ src/main/java/io/github/moonstroke/once/Requirements.java
@@ -0,0 +1,11 @@
+package io.github.moonstroke.once;
+
+/**
+ * Defines a set of common requirements, for various types.
+ */
+public class Requirements {
+
+	private Requirements() {
+		/* Static-only utilities class */
+	}
+}
