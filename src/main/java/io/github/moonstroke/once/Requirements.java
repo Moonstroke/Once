5b5c046a0a3687bf1c523049b0c7d203fa5a26f6
Joachim "Moonstroke" MARIE <jiema@live.fr>
2025-08-02 17:12:47 +0200

    Define common string requirements
    The requirements correspond to a non-empty and a non-blank check on a
    string.

--- src/main/java/io/github/moonstroke/once/Requirements.java
+++ src/main/java/io/github/moonstroke/once/Requirements.java
@@ -8,4 +8,11 @@ public class Requirements {
 	private Requirements() {
 		/* Static-only utilities class */
 	}
+
+
+	public static final Requirement<String> STRING_NOT_EMPTY = Requirement.fromPredicate(str -> !str.isEmpty(),
+	                                                                                     "value cannot be an empty string");
+
+	public static final Requirement<String> STRING_NOT_BLANK = Requirement.fromPredicate(str -> !str.isBlank(),
+	                                                                                     "value cannot be a blank string");
 }
