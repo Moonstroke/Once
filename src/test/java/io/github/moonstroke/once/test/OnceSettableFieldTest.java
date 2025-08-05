4529cd5e6a6f236da63dbcccdabd685f0070566d
Joachim MARIE <joachim.marie-d-avigneau@csgroup.eu>
2025-08-05 15:22:08 +0200

    Thoroughly test methods setFrom and trySetFrom

--- src/test/java/io/github/moonstroke/once/test/OnceSettableFieldTest.java
+++ src/test/java/io/github/moonstroke/once/test/OnceSettableFieldTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.Test;
 
@@ -44,6 +45,57 @@ class OnceSettableFieldTest {
 	}
 
 	@Test
+	void testCallToSetFromNullFails() {
+		OnceSettableField<Object> once = new OnceSettableField<>("field");
+		assertThrows(NullPointerException.class, () -> once.setFrom(null));
+	}
+
+	@Test
+	void testCallToSetFromNullSuppliedFails() {
+		OnceSettableField<Object> once = new OnceSettableField<>("field");
+		assertThrows(NullPointerException.class, () -> once.setFrom(() -> null));
+	}
+
+	@Test
+	void testCallToSetFromNonNullSucceeds() {
+		OnceSettableField<Object> once = new OnceSettableField<>("field");
+		assertDoesNotThrow(() -> once.setFrom(Object::new));
+	}
+
+	@Test
+	void testCallToSetFromCallsSupplier() {
+		OnceSettableField<Object> once = new OnceSettableField<>("field");
+		boolean[] called = new boolean[1];
+		assertDoesNotThrow(() -> once.setFrom(() -> {
+			called[0] = true;
+			return new Object();
+		}));
+		assertTrue(called[0]);
+	}
+
+	@Test
+	void testCallToSetFromSetsSuppliedValue() {
+		OnceSettableField<Object> once = new OnceSettableField<>("field");
+		Object value = new Object();
+		assertDoesNotThrow(() -> once.setFrom(() -> value));
+		assertEquals(value, once.get());
+	}
+
+	@Test
+	void testCallToSetFromFailsIfAlreadySet() {
+		OnceSettableField<Object> once = new OnceSettableField<>("field");
+		once.set(new Object());
+		assertThrows(IllegalStateException.class, () -> once.setFrom(Object::new));
+	}
+
+	@Test
+	void testCallToSetFromDoesNotCallSupplierIfAlreadySet() {
+		OnceSettableField<Object> once = new OnceSettableField<>("field");
+		once.set(new Object());
+		assertThrows(IllegalStateException.class, () -> once.setFrom(() -> fail("supplier should not be called")));
+	}
+
+	@Test
 	void testCallToTrySetNullFails() {
 		OnceSettableField<Object> once = new OnceSettableField<>("field");
 		assertThrows(NullPointerException.class, () -> once.trySet(null));
@@ -60,6 +112,57 @@ class OnceSettableFieldTest {
 		OnceSettableField<Object> once = new OnceSettableField<>("field");
 		once.set(new Object());
 		assertFalse(once.trySet(new Object()));
+	}
+
+	@Test
+	void testCallToTrySetFromNullFails() {
+		OnceSettableField<Object> once = new OnceSettableField<>("field");
+		assertThrows(NullPointerException.class, () -> once.trySetFrom(null));
+	}
+
+	@Test
+	void testCallToTrySetFromNullSuppliedFails() {
+		OnceSettableField<Object> once = new OnceSettableField<>("field");
+		assertThrows(NullPointerException.class, () -> once.trySetFrom(() -> null));
+	}
+
+	@Test
+	void testCallToTrySetFromNonNullSucceeds() {
+		OnceSettableField<Object> once = new OnceSettableField<>("field");
+		assertTrue(once.trySetFrom(Object::new));
+	}
+
+	@Test
+	void testCallToTrySetFromCallsSupplier() {
+		OnceSettableField<Object> once = new OnceSettableField<>("field");
+		boolean[] called = new boolean[1];
+		assertTrue(once.trySetFrom(() -> {
+			called[0] = true;
+			return new Object();
+		}));
+		assertTrue(called[0]);
+	}
+
+	@Test
+	void testCallToTrySetFromSetsSuppliedValue() {
+		OnceSettableField<Object> once = new OnceSettableField<>("field");
+		Object value = new Object();
+		assertTrue(once.trySetFrom(() -> value));
+		assertEquals(value, once.get());
+	}
+
+	@Test
+	void testCallToTrySetFromReturnsFalseIfAlreadySet() {
+		OnceSettableField<Object> once = new OnceSettableField<>("field");
+		once.set(new Object());
+		assertFalse(once.trySetFrom(Object::new));
+	}
+
+	@Test
+	void testCallToTrySetFromDoesNotCallSupplierIfAlreadySet() {
+		OnceSettableField<Object> once = new OnceSettableField<>("field");
+		once.set(new Object());
+		assertFalse(once.trySetFrom(() -> fail("supplier should not be called")));
 	}
 
 	@Test
