f48de589f8216eda4a377547e9d8a79441cbdb83
Joachim MARIE <moonstroke+github@live.fr>
2025-08-22 18:42:23 +0200

    Add module descriptor
    The module consists of the io.github.moonstroke.once package; it is
    named identically and only exports this package.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,6 @@
+/* SPDX-FileCopyrightText: 2025 (c) Joachim MARIE <moonstroke+github@live.fr>
+ * SPDX-License-Identifier: MIT */
+module io.github.moonstroke.once {
+
+	exports io.github.moonstroke.once;
+}
