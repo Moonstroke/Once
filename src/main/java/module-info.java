989609f12256ec0bdb82a62e8122f69344021982
Joachim MARIE <moonstroke+github@live.fr>
2025-08-23 11:24:50 +0200

    Document the module

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,5 +1,8 @@
 /* SPDX-FileCopyrightText: 2025 (c) Joachim MARIE <moonstroke+github@live.fr>
  * SPDX-License-Identifier: MIT */
+/**
+ * A module providing a container allowing a unique initialization.
+ */
 module io.github.moonstroke.once {
 
 	exports io.github.moonstroke.once;
